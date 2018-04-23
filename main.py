3650cfccb0c4085dd916df02f594ce3078baf52a
Craig Nishina <craig.nishina@gmail.com>
2018-04-23 16:53:13 -0700

    An unusual refactor

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ import requests
 
 _URL = 'https://www.metaweather.com/api/location/'
 
-# Get the woeid
-location_id = str(
-  requests.get(_URL + 'search/?query=seattle').json()[0]['woeid'])
+def get_weather_state_by_location(location):
+  # Get the woeid
+  location_id = str(
+    requests.get(_URL + 'search/?query=' + location).json()[0]['woeid'])
 
-# Weather and weather state
-weather = requests.get(_URL + location_id +'/').json()
-weather_state = weather['consolidated_weather'][0]['weather_state_name']
+  # Weather and weather state
+  weather = requests.get(_URL + location_id +'/').json()
+  weather_state = weather['consolidated_weather'][0]['weather_state_name']
+  return weather_state
+
+
+def get_weather_state_by_woeid(location_id):
+  # Weather and weather state
+  weather = requests.get(_URL + location_id +'/').json()
+  weather_state = weather['consolidated_weather'][0]['weather_state_name']
+  return weather_state
 
 # Log
-print 'location id: %s' % location_id
-print 'weather state: %s' % weather_state
+print 'weather state: %s' % get_weather_state_by_location('seattle')
+print 'weather state: %s' % get_weather_state_by_woeid('2490383')
 
 # TODO(cnishina): Convert to functions and add testing.
 # TODO(cnishina): Add option to write to file.
